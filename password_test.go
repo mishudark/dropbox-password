14de50163bc3e1cb9a2918d3781d2044b9281599
mishel <mishu.drk@gmail.com>
2016-12-26 11:29:09 -0600

    refactor(test): better interline

--- password_test.go
+++ password_test.go
@@ -16,7 +16,6 @@ func TestHash(t *testing.T) {
 
 func TestIsValid(t *testing.T) {
 	ok := IsValid("mishudark", "aes256$mh68GJ7t9mLYiJKk$7ab2234944dabe98db01e7e6cabeb47cc32d255bdf9bd93ac0790624ca5177372fadf1c403b002867d7d94e8a1f892d0ef3b266a72771a053074e7b59b8b3d7d2161110675bb0794a80f8952", "AES256Key-32Characters1234567890")
-
 	if !ok {
 		t.Errorf("should be true got false")
 	}
